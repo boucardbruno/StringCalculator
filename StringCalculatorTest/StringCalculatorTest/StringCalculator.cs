d8cbde8a9de00dfc8ff35b94f8586b6a0bf56656
Bruno Boucard <boucard.bruno@free.fr>
2015-09-23 22:14:56 +0200

    Refactoring StringCalculator class

--- StringCalculatorTest/StringCalculatorTest/StringCalculator.cs
+++ StringCalculatorTest/StringCalculatorTest/StringCalculator.cs
@@ -1,18 +1,15 @@
+using System;
+using System.Linq;
+
 namespace StringCalculatorTest
 {
     public class StringCalculator
     {
         public int Add(string input)
         {
-            if (string.IsNullOrEmpty(input))
-                return 0;
-            int sum = 0;
-            var numbers = input.Split(',');
-            foreach (var number in numbers)
-            {
-                sum += int.Parse(number);
-            }
-            return sum;
+            var numbers = input.Split(new []{','}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
+
+            return numbers.Sum();
         }
     }
 }

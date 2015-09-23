9067eb76780c74d45b00213a768cb222d1215e0d
Bruno Boucard <boucard.bruno@free.fr>
2015-09-23 21:54:32 +0200

    Should return zero when input is empty

---
+++ StringCalculatorTest/StringCalculatorTest/StringCalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculatorTest
+{
+    public class StringCalculatorTests
+    {
+
+        [Test]
+        public void Should_return_zero_when_input_is_empty()
+        {  
+            StringCalculator stringCalculator = new StringCalculator();
+
+            int actual = stringCalculator.Add(string.Empty);
+            int expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class StringCalculator
+    {
+        public int Add(string input)
+        {
+            return 0;
+        }
+    }
+}
